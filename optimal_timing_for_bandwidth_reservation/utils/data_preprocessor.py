5bb97932e0ca5165678ab184c47014741fe771a1
Qubut <qubut@tutanota.com>
2023-10-28 07:35:11 +0200

    feat: optimized datapreprocessing

--- optimal_timing_for_bandwidth_reservation/utils/data_preprocessor.py
+++ optimal_timing_for_bandwidth_reservation/utils/data_preprocessor.py
@@ -65,36 +65,33 @@ class DataPreProcessor:
             df = df.drop(columns=["Instance Type", "Region"])
             df = df.rename(columns={"Price": f"Price_{idx}"}).set_index("Date")
             dfs.append(df)
-
-        main_df = dd.concat(dfs, axis=1, interleave_partitions=True).reset_index()
-
+        if len(dfs) > 1:
+            main_df = dd.concat(dfs, axis=1, interleave_partitions=True).reset_index()
+        else:
+            main_df = dfs[0]
         main_df = main_df.fillna(method="ffill").fillna(method="bfill")
 
         # Convert timestamp to unix timestamp (seconds since epoch)
         main_df["timestamp_unix"] = (
             main_df["Date"].astype("M8[ns]").astype("int64") // 10**9
         )
-        features = main_df[["timestamp_unix"]].to_dask_array(lengths=True)
-        prices = main_df.drop(columns=["Date", "timestamp_unix"]).to_dask_array(
-            lengths=True
-        )
+        price_columns = [col for col in main_df.columns if "Price_" in col]
+        for col in price_columns:
+            main_df[col] = (
+                main_df[col]
+                .to_dask_array(lengths=True)
+                .map_blocks(self.scaler.transform, dtype=float)
+            )
 
-        prices_normalized = (
-            self.scaler.transform(prices.compute().ravel())
-            .reshape(prices.shape)
-            .compute()
-        )
-        data = dd.from_array(dd.concatenate([features, prices_normalized], axis=1))
-
-        train_size = dd.from_delayed(len(data) * self.train_ratio).compute()
-        val_size = dd.from_delayed(train_size * self.validation_ratio).compute()
+        train_size = int(len(main_df) * self.train_ratio)
+        val_size = int(train_size * self.validation_ratio)
         _train_size = train_size - val_size
 
-        self.train_data = data.loc[: _train_size - 1].compute().to_numpy()
+        self.train_data = main_df.loc[: _train_size - 1].compute().to_numpy()
         self.validation_data = (
-            data.loc[_train_size : _train_size + val_size - 1].compute().to_numpy()
+            main_df.loc[_train_size : _train_size + val_size - 1].compute().to_numpy()
         )
-        self.test_data = data.loc[_train_size + val_size :].compute().to_numpy()
+        self.test_data = main_df.loc[_train_size + val_size :].compute().to_numpy()
 
     def _create_inout_sequences(
         self, data: np.ndarray
