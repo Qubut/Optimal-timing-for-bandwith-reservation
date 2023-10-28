115880a13600ada33ea0c9f2a314676c4f84335e
Qubut <qubut@tutanota.com>
2023-10-28 06:59:49 +0200

    fix: correct pathw

--- optimal_timing_for_bandwidth_reservation/config/params.py
+++ optimal_timing_for_bandwidth_reservation/config/params.py
@@ -19,12 +19,12 @@ class Params:
     N_HIDDEN_L: int = 100
     DATAFILES: List[str] = field(
         default_factory=lambda: [
-            "../datasets/Dataset_NO1.csv",
-            "../datasets/Dataset_NO2.csv",
-            "../datasets/Dataset_NO3.csv",
+            "./datasets/Dataset_NO1.csv",
+            "./datasets/Dataset_NO2.csv",
+            "./datasets/Dataset_NO3.csv",
         ]
     )
-    LSTM_RESULTS_PATH: str = "../out/lstm"
-    TRANSFORMER_RESULTS_PATH: str = "../out/transfomer"
-    SAVED_MODELS_PATH: str = "../out/models"
-    RUNS_LOG_PATH: str = "../runs"
+    LSTM_RESULTS_PATH: str = "./out/lstm"
+    TRANSFORMER_RESULTS_PATH: str = "./out/transfomer"
+    SAVED_MODELS_PATH: str = "./out/models"
+    RUNS_LOG_PATH: str = "./runs"
