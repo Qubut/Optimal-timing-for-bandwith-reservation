768d4cecb71d262a9c16d182b105084215f4dc1d
Qubut <qubut@tutanota.com>
2023-10-28 06:58:54 +0200

    fix: correct datasets path

--- optimal_timing_for_bandwidth_reservation/config/params.py
+++ optimal_timing_for_bandwidth_reservation/config/params.py
@@ -19,9 +19,9 @@ class Params:
     N_HIDDEN_L: int = 100
     DATAFILES: List[str] = field(
         default_factory=lambda: [
-            "./datasets/Dataset_NO1.csv",
-            "./datasets/Dataset_NO2.csv",
-            "./datasets/Dataset_NO3.csv",
+            "../datasets/Dataset_NO1.csv",
+            "../datasets/Dataset_NO2.csv",
+            "../datasets/Dataset_NO3.csv",
         ]
     )
     LSTM_RESULTS_PATH: str = "../out/lstm"
